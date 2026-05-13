cc3ee3550034c103bd1ecf507e2bd24341bd898a
gyanamchoudhary <gyanamchoudhary@users.noreply.github.com>
2026-05-13 18:56:53 +0530

    style: switch back to logo.png with background for better contrast

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
           <div className="lg:col-span-1">
             <Link to="/" className="flex items-center mb-4">
               <img
-                src="/images/logo_transparent.png"
+                src="/images/logo.png"
                 alt="Harshollasha"
                 className="h-24 lg:h-28 w-auto object-contain"
               />
